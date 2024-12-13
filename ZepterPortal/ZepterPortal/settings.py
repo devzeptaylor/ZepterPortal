3d23919a12259c960d5e1092029a4c00836230dd
sholpan.baitursynova <adil.anizuly@zepter.kz>
2024-12-13 14:11:15 +0500

    changing to postgres

--- ZepterPortal/ZepterPortal/settings.py
+++ ZepterPortal/ZepterPortal/settings.py
@@ -74,12 +74,15 @@ WSGI_APPLICATION = 'ZepterPortal.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   	'default': {
+       		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       		 'NAME': 'postgres',
+       		 'USER': 'postgres',
+       		 'PASSWORD': '123456',
+      		 'HOST': 'localhost',
+       		 'PORT': '',
+   	}
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
