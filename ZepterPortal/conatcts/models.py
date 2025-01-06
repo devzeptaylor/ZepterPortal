51fd2b6393a7ad6bf0abb9e29027faaf6d81efbd
devzeptaylor@gmail.com <devzeptaylor@gmail.com>
2025-01-06 11:01:42 +0500

    telefone

--- ZepterPortal/conatcts/models.py
+++ ZepterPortal/conatcts/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Telefon(models.Model):
+    fio = models.CharField(max_length=255)
+    tel = models.CharField(max_length=25)
+    mobile = models.CharField(max_length=25)
+    user = models.ForeignKey(User,on_delete=models.SET_NULL, null=True)
+
