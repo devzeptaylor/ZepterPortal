9f3c587d14f6758f07674fd1139ebafd413e69da
devzeptaylor@gmail.com <devzeptaylor@gmail.com>
2025-01-18 16:59:54 +0500

    add : env

--- ZepterPortal/ZepterPortal/settings.py
+++ ZepterPortal/ZepterPortal/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import logging
 from pathlib import Path
+import os
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,6 +78,13 @@ WSGI_APPLICATION = 'ZepterPortal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# .env
+
+from dotenv import load_dotenv
+
+# Загружаем переменные окружения из файла .env
+#load_dotenv()
+
 DATABASES = {
    	'default': {
        		'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -85,6 +95,8 @@ DATABASES = {
        		 'PORT': '5432',
    	}
 }
+
+#print(os.getenv('DATABASE_NAME', 'default_db_name'))
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
